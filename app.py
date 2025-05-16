f66a1f6bfddcf4d2a942c0514225809fb97be80b
saadserghini-escp <saad.serghini@edu.escp.eu>
2025-05-16 16:24:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
+st.set_page_config(page_title="Financial Deals Dashboard", page_icon="💰")
 
 # ----------------------- CHARGEMENT DU FICHIER -----------------------
 try:
